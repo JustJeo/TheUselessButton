79c841bc31ba4051775d5bb014013d126c087c1b
JustJeo <j.cabrigas.writer@gmail.com>
2021-03-25 19:20:37 -0700

    codes in responses

---
+++ app.js
@@ -0,0 +1,37 @@
+// Connection check
+console.log("AYO! This is Jeo!")
+
+// Fired function
+  // User clicks button
+
+  // Button relays response
+    // Button randomly chooses a response
+    // Displays snarky response to user
+  // Button turns OFF
+    // Button changes back to red with "OFF"
+
+let game = {
+  buttonChange: function() {
+      // Button turns ON
+        // Button changes to blue with "ON"
+  },
+
+  response: function() {
+    let randomResponse = Math.floor(Math.random()*5);
+    if (randomResponse === 4) {
+      document.getElementsByClassName("response").innerHTML = "HEY! Who turned on the light?! I'm sleeping here!"
+    } else if (randomResponse === 3) {
+      document.getElementsByClassName("response").innerHTML = "Shhhh. We're watching a movie."
+    } else if (randomResponse === 2) {
+      document.getElementsByClassName("response").innerHTML = "It's 3am in here!"
+    } else if (randomResponse === 1) {
+      document.getElementsByClassName("response").innerHTML = "TA-DAAAA.... I'm done."
+    } else {
+      document.getElementsByClassName("response").innerHTML = "...No."
+    }
+  }
+}
+
+function clickedButton() {
+
+}
